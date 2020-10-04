345629b267a4e9f5f8cb7dcd20f87aa2af730a4b
Steven Tupper <steven.arthur.tupper@gmail.com>
2020-10-04 11:30:40 +0200

    Make cover data generation a bit more dynamic

--- Assets/Scripts/Demo/NavmeshCoverDataGenerator.cs
+++ Assets/Scripts/Demo/NavmeshCoverDataGenerator.cs
@@ -7,21 +7,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.Demo
 {
     public class NavmeshCoverDataGenerator : NavmeshMetaDataProviderBase<bool>
     {
+        [SerializeField]
+        private Transform playerLookStart;
+        [SerializeField]
+        private Transform playerLookEnd;
+        [SerializeField]
+        private float playerFOV = 60;
+
         internal override IEnumerator UpdateNavmeshMetaData(IOctreeReadonly<NavmeshTriangle<bool>> triangles)
         {
             var allTriangles = triangles.GetAllContents();
 
-            for (var i = 0; i < allTriangles.Count; ++i)
+            while (true)
             {
-                allTriangles[i].MetaData = i % 2 == 0;
-            }
+                var playerLookDirection = playerLookEnd.position - playerLookStart.position;
 
-            yield return null;
+                foreach (var currentTri in allTriangles)
+                {
+                    currentTri.MetaData = true;
+
+                    var closestPointOnTriPlane = currentTri.Plane.ClosestPointOnPlane(playerLookStart.position);
+
+                    if (currentTri.ContainsPoint(closestPointOnTriPlane) && Vector3.Angle(playerLookDirection, closestPointOnTriPlane - playerLookStart.position) < playerFOV)
+                    {
+                        currentTri.MetaData = false;
+                    } else
+                    {
+                        var corners = new[] { currentTri.Corner1, currentTri.Corner2, currentTri.Corner3 };
+
+                        for (var i = 0; i < 3; ++i)
+                        {
+                            var closestPointOnSide = Utils.Utils.ClosestPointOnLineSegment(corners[i], corners[(i + 1) % 3], playerLookStart.position);
+
+                            if (Vector3.Angle(playerLookDirection, closestPointOnSide - playerLookStart.position) < playerFOV)
+                            {
+                                currentTri.MetaData = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                yield return null;
+            }
         }
     }
 }
