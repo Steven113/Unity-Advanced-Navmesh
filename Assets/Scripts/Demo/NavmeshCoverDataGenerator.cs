99ecd26dcd925ce7bc771bb2829490b41c8f7230
Steven Tupper <steven.arthur.tupper@gmail.com>
2020-10-04 11:56:45 +0200

    Raycast against all corners of triangle for better cover data

--- Assets/Scripts/Demo/NavmeshCoverDataGenerator.cs
+++ Assets/Scripts/Demo/NavmeshCoverDataGenerator.cs
@@ -36,41 +36,31 @@ namespace Assets.Scripts.Demo
 
                     var corners = new[] { currentTri.Corner1, currentTri.Corner2, currentTri.Corner3 };
 
-                    Vector3? coverCheckPoint = null;
+                    var protectedSides = 0;
 
                     for (var i = 0; i < 3; ++i)
                     {
                         var closestPointOnSide = (corners[i] + corners[(i + 1) % 3]) /2f;
 
-                        //if (Vector3.Angle(closestPointOnSide - playerLookStart.position, playerLookDir) > playerFOV)
-                        //{
-                        //    continue;
-                        //}
-                        //else if (Vector3.Distance(closestPointOnSide, playerLookStart.position) > maxPlayerRange)
-                        //{
-                        //    continue;
-                        //}
-
-                        if (!coverCheckPoint.HasValue || Vector3.Distance(coverCheckPoint.Value, playerLookStart.position) > Vector3.Distance(closestPointOnSide, playerLookStart.position))
+                        if (Vector3.Angle(closestPointOnSide - playerLookStart.position, playerLookDir) > playerFOV)
                         {
-                            coverCheckPoint = closestPointOnSide;
+                            ++protectedSides;
+                        }
+                        else if (Vector3.Distance(closestPointOnSide, playerLookStart.position) > maxPlayerRange)
+                        {
+                            ++protectedSides;
+                        }
+                        else
+                        {
+                            if (Physics.Raycast(playerLookStart.position, closestPointOnSide - playerLookStart.position, out var hitInfo, maxPlayerRange)
+                                && Vector3.Distance(hitInfo.point, playerLookStart.position) < Vector3.Distance(closestPointOnSide, playerLookStart.position))
+                            {
+                                ++protectedSides;
+                            }
                         }
                     }
 
-                    if (Vector3.Angle(coverCheckPoint.Value - playerLookStart.position, playerLookDir) > playerFOV)
-                    {
-                        currentTri.MetaData = true;
-                    } else if (Vector3.Distance(coverCheckPoint.Value, playerLookStart.position) > maxPlayerRange)
-                    {
-                        currentTri.MetaData = true;
-                    } else
-                    {
-                        if (Physics.Raycast(playerLookStart.position, coverCheckPoint.Value - playerLookStart.position, out var hitInfo, maxPlayerRange)
-                            && Vector3.Distance(hitInfo.point, playerLookStart.position) < Vector3.Distance(coverCheckPoint.Value, playerLookStart.position))
-                        {
-                            currentTri.MetaData = true;
-                        }
-                    }
+                    currentTri.MetaData = protectedSides == 3;
                 }
 
                 yield return null;
